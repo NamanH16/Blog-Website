1b4ed3f1e5b2eacfda12fef83fb0dc9c7ffec0f9
Naman Hiran <72915205+BloodyMary479@users.noreply.github.com>
2022-10-05 01:25:00 +0530

    Connect mongoDB

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const _ = require('lodash');
-//const ejs = require("ejs");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,19 +15,44 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));  // tell app that my static files are present in the public folder 
 
-let posts = []; 
+//connect to MongoDB by specifying port to access MongoDB server
+main().catch(err => console.log(err));
+ 
+async function main() {
+   mongoose.connect("mongodb://localhost:27017/blogDB",{useNewUrlParser:true});
+};
+
+// CREATE SCHEMA
+const postSchema = {
+  title:String,
+  content:String
+};
+
+// CREATE MODEL
+const Post = mongoose.model("Post",postSchema);
 
 app.post("/compose",function(req,res){
-  const post = {
+  
+  // CREATE DOCUMENT
+  const post = new Post({
     title: req.body.titleBox,
     content:req.body.postBox
-  };
-  posts.push(post);
-  res.redirect("/");
+  });
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  });
 });
 
 app.get("/",function(req,res){
-  res.render("home",{startingContent:homeStartingContent, posts:posts} );
+  Post.find({},function(err, posts){
+    if(!err){
+      res.render("home",{startingContent:homeStartingContent, posts:posts} );
+    }else{
+      console.log(err);
+    }
+  });
 });
 
 app.get("/about",function(req,res){
@@ -41,16 +67,14 @@ app.get("/compose",function(req,res){
   res.render("compose");
 });
 
-app.get("/posts/:postName",function(req,res){
-  const requestedTitle = req.params.postName;
-  posts.forEach(function(post){
-      const storedTitle = post.title;
-
-    if(_.lowerCase(storedTitle) === _.lowerCase(requestedTitle)){
-      res.render("post",{title:post.title, content: post.content});
-    }
+app.get("/posts/:postId",function(req,res){
+  const requestedPostId = req.params.postId;
+  Post.findOne({_id:requestedPostId},function(err,post){
+    res.render("post",{
+      title:post.title,
+      content:post.content
+    });
   });
-
 });
 
 
